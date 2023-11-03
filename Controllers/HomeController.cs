76b3c17c17315ad43d8cbdea5a214c2d08387dff
Paritosh Sabade <paritoshsabade@Paritoshs-MacBook-Air.local>
2023-11-03 00:55:51 -0400

    intial push

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using MVCProject.Models;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace MVCProject.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: /<controller>/
+        public IActionResult Index(Student student)
+        {
+            return Content($"{student}");
+        }
+    }
+}
+
---
+++ Models/Student.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace MVCProject.Models
+{
+	public class Student
+	{
+
+        [Required]
+        [StringLength(100)]
+        public string? Name { get; set; }
+
+        
+
+        [Required]
+        [EmailAddress]
+        public string? Email { get; set; }
+
+        [Required]
+        [Phone]
+        public string? PhoneNumber { get; set; }
+
+        [Required]
+        [DataType(DataType.Date)]
+        [Display(Name = "Date of Birth")]
+        [DisplayFormat(DataFormatString = "{0:MM/dd/yyyy}", ApplyFormatInEditMode = true)]
+        [CustomDateOfBirthValidator] // You'll define this custom validation attribute later.
+        public DateTime DateOfBirth { get; set; }
+
+        [Required]
+        [MinLength(8)]
+        [RegularExpression(@"^(?=.*[a-z])(?=.*[A-Z])(?=.*\d).+$", ErrorMessage = "Password must meet the criteria.")]
+        [DataType(DataType.Password)]
+        public string? Password { get; set; }
+
+        [Required]
+        [Compare("Password", ErrorMessage = "The password and confirmation password do not match.")]
+        [DataType(DataType.Password)]
+        [Display(Name = "Confirm Password")]
+        public string? ConfirmPassword { get; set; }
+
+
+        public override string ToString()
+        {
+            return $"Student Object contains: Name: {Name},Email: {Email}, Phone No: {PhoneNumber}, Password: {Password}";
+        }
+    }
+}
+
